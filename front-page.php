6d90aec895181b8707e610bb808c6d8ad7798c5e
Remy Zagorski <rszagorski@gmail.com>
2016-11-02 09:59:47 -0400

    i started woohoo

--- front-page.php
+++ front-page.php
@@ -6,7 +6,15 @@
     <div class="content">
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
+      <?php 
+        $bgImage = get_field('background_image');
+      ?>
+      <header class="hero" style="background-image:url(<?php echo $bgImage['url'] ?>">
+        <?php $headerLogo = the_field('header_logo')?>
+        <img src="<?php echo $headerLogo['url'] ?>" alt="<?php echo $headerLogo['alt'] ?>"/>
+        <h1><?php the_field('header_text') ?></h1> 
+        <?php the_content(); ?>
+      </header>
 
       <?php endwhile; // end the loop?>
     </div> <!-- /,content -->
